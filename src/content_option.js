444000cfe0f22481bb3bcef90382bd1307806027
GIT_GUI_DEMO <harshsingh2298@gmail.com>
2024-07-05 15:13:20 +0530

    updated

---
+++ src/content_option.js
@@ -0,0 +1,166 @@
+const logotext = "HSR";
+const meta = {
+    title: "Harsh Singh",
+    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Harsh Singh Raghuvanshi",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "Transforming ideas into dynamic digital solutions with seasoned expertise in Java full-stack development.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "A bit about myself",
+    aboutme: "I am Harsh Singh Raghuvanshi, a skilled Full Stack Java Developer with a passion for transforming ideas into dynamic digital solutions. Based in Bangalore, India, I specialize in front-end and back-end development, database management, and various development tools. My expertise includes Java, Spring Boot, React.js, Redux, and other modern technologies. I thrive in collaborative environments and am committed to continuous learning and improvement in the tech field.",
+};
+const worktimeline = [
+    {
+        jobtitle: "Full Stack Java Developer",
+        where: "CMS Computers Ltd",
+        date: "2023 - Present",
+        description: "Enhancing application performance and user experience through full-stack development."
+    },
+    {
+        jobtitle: "Trainee Developer",
+        where: "NIIT Technologies LTD",
+        date: "2022 - 2023",
+        description: "Developed and maintained web applications using Java, Spring Boot, and React.js."
+    },
+    {
+        jobtitle: "Intern",
+        where: "NIIT Technologies LTD",
+        date: "2022 - 2023",
+        description: "Assisted in building backend services and APIs using Java and Spring framework and Angular."
+    }
+];
+
+
+const skills = [
+    { name: "Java", value: 90 },
+    { name: "Spring Boot", value: 85 },
+    { name: "JavaScript", value: 60 },
+    { name: "React", value: 70 },
+    { name: "JQuery", value: 85 },
+    { name: "Microservices Architecture", value: 90 },
+    { name: "Database Management", value: 70 },
+    { name: "AWS (S3, RDS, CodeCommit, EC2, IAM)", value: 68 },
+];
+
+const services = [
+    {
+      title: "Front-end Development",
+      description: "Creating responsive and dynamic user interfaces using React.js, Redux, HTML5, CSS3, and JavaScript (ES6+).",
+    },
+    {
+      title: "Back-end Development",
+      description: "Designing and developing robust server-side applications with Spring Boot, RESTful API Design, Microservices Architecture, MVC, and Hibernate.",
+    },
+    {
+      title: "Database Management",
+      description: "Optimizing data storage and retrieval using MySQL, MongoDB, PostgreSQL, and Oracle SQL.",
+    },
+    {
+      title: "Development Tools",
+      description: "Implementing efficient development workflows with Git, Docker, Jenkins CI/CD, AWS (S3, RDS, Code Commit, EC2, IAM), and JIRA.",
+    },
+    {
+      title: "UI & UX Design",
+      description: "Enhancing user interfaces with intuitive designs to improve user experience and engagement.",
+    },
+    {
+      title: "Mobile App Development",
+      description: "Creating mobile applications with responsive and user-friendly interfaces tailored to client needs.",
+    },
+    {
+      title: "Agile Methodologies",
+      description: "Implementing agile practices to ensure timely project delivery and continuous improvement.",
+    },
+    {
+      title: "Application Security",
+      description: "Integrating robust security measures, such as OAuth 2.0 and JWT tokens, to protect server-side APIs and ensure application security.",
+    },
+    {
+      title: "Continuous Integration and Deployment (CI/CD)",
+      description: "Setting up CI/CD pipelines to automate software delivery processes, ensuring faster and more reliable deployments.",
+    },
+  ];
+  
+  const dataportfolio = [
+    {
+        img: "https://c8.alamy.com/comp/2F3X53A/e-commerce-concept-top-layout-of-drawings-of-laptops-notepads-coffee-different-business-stuff-on-black-background-with-inscription-e-commerce-2F3X53A.jpg",
+        description: "E-Commerce-For-Shopify-App-With-Payment-Integration-Razorpay",
+        link: "https://github.com/harshsingh2298/E-Commerce-For-Shoppify-App-With-Payment-Integration-Razorpay",
+    },
+    {
+        img: "https://c8.alamy.com/comp/2GDGXNF/air-ticket-booking-concept-illustrated-by-a-picture-on-background-2GDGXNF.jpg",
+        description: "Flight-Ticket-Booking-Application-Using-Buffer-Reader-and-Stream-API",
+        link: "https://github.com/harshsingh2298/Flight-Ticket-Booking-Application-Using-Buffer-Reader-and-stream-API",
+    },
+    {
+        img: "https://www.qrcodepress.com/wp-content/uploads/2014/04/qr-code-marketing.jpg",
+        description: "Java-Two-Factor-Authentication-Using-OTP-and-QR-Code",
+        link: "https://github.com/harshsingh2298/Java-Two-Factor-Authentication-Using-OTP-and-QR-Code",
+    },
+    {
+        img: "https://www.shutterstock.com/image-vector/word-pdf-ppt-excel-document-260nw-2141682571.jpg",
+        description: "Import-and-Export-Excel-Sheet-Reader-Using-REACT-JS",
+        link: "https://github.com/harshsingh2298/Import-and-Export-Excel-Sheet-Reader-Using-REACT-JS-",
+    },
+    {
+        img: "https://i.pinimg.com/originals/19/39/f5/1939f544e0007c816347e231600e923c.gif",
+        description: "Employee-Management-System-Reactjs-and-Spring-Boot",
+        link: "https://github.com/harshsingh2298/Employee-Management-System-Reactjs-and-Spring-Boot-",
+    },
+    {
+        img: "https://cdn.venngage.com/template/thumbnail/small/0614a52c-7532-4f5f-871b-1e1827b2f4a9.webp",
+        description: "Dynamic-Form-React-js-and-SpringBoot",
+        link: "https://github.com/harshsingh2298/Dynamic-Form-React-js-and-SpringBoot",
+    },
+    {
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9tkybyr3hzYB37eY_5GVROTRPzF9saruyrg&s",
+        description: "IDBC-Bank-Replication-Using-Java-and-MySQL",
+        link: "https://github.com/harshsingh2298/IDBC-Bank-Replication-Using-Java-and-MySQL",
+    },
+    {
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWa1jj1Nil6cySCZ0MB5GjWXFkG-gOShxkwg&s",
+        description: "Juke-Box-Music-Player-Using-JAVA",
+        link: "https://github.com/harshsingh2298/Juke-Box-Music-Player-Using-JAVA",
+    },
+];
+
+export default dataportfolio;
+
+
+const contactConfig = {
+    YOUR_EMAIL: "harshsingh2298@gmail.com",
+    YOUR_FONE: "+91-9106198825",
+    description: "Connecting dots between innovation and efficiency, with a passion for crafting seamless digital experiences. Let's collaborate and bring your ideas to life!",
+    YOUR_SERVICE_ID: "service_bnd3w8w",
+    YOUR_TEMPLATE_ID: "template_a45et2m",
+    YOUR_USER_ID: "bV3EO2aD6bqgCRaZW",
+};
+
+const socialprofils = {
+    github: "https://github.com/harshsingh2298",
+    facebook: "https://facebook.com/harshsingh2298",
+    linkedin: "https://www.linkedin.com/in/harsh-singh-raghuvanshi-a3ab6b122/",
+    twitter: "https://twitter.com/Harshsingh2298",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
